eedd02f27018b991d7da079462ae5f4f54c99d3d
Iwis Z <iwiszhou@gmail.com>
2019-12-09 16:15:27 -0500

    update

--- yelp-dataset-nlp.py
+++ yelp-dataset-nlp.py
@@ -26,7 +26,7 @@ import spacy
 from collections import Counter
 
 # Download stopwords if not existing
-nltk.download('stopwords')
+# nltk.download('stopwords')
 
 # Set col to max width
 pd.set_option('display.max_colwidth', -1)
@@ -37,6 +37,7 @@ table_names = {
     "reviews": "reviews",
     "clean_reviews": "clean_reviews"
 }
+
 
 # Helper functions
 def get_absolute_path(file_name):
@@ -245,7 +246,7 @@ top_business_counter = Counter(remove_stopwords(top_business_tokens))
 top_business_counter.most_common(20)
 
 # Convert tuples to data frame
-top_business_freq = pd.DataFrame.from_records(top_business_counter.most_common(20), columns=['token','count'])
+top_business_freq = pd.DataFrame.from_records(top_business_counter.most_common(20), columns=['token', 'count'])
 
 # Create bar plot
 top_business_freq.plot.bar(x="token")
@@ -257,7 +258,9 @@ belong to a high rating and they are happy with the service.
 So that, we could based on the 'star' to classify to Positive or Negative.
 '''
 
-# Even though, we have some sort of conclusion, but let's use wordcloud to make the plot more visible and easier to understand
+# Even though, we have some sort of conclusion, but let's use wordcloud to make the plot more visible and easier to
+# understand
+
 from wordcloud import WordCloud
 
 
@@ -265,23 +268,28 @@ def wordcloud(counter):
     wc = WordCloud(width=1200, height=800, background_color='white', max_words=200)
     wc.generate_from_frequencies(counter)
 
-    #plot
-    fig = plt.figure(figsize=(6,4))
+    # plot
+    fig = plt.figure(figsize=(6, 4))
     plt.imshow(wc, interpolation="none")
     plt.axis("off")
-    plt.tight_layout(pdf=0)
+    plt.tight_layout(pad=0)
     plt.show()
+
 
 wordcloud(top_business_counter)
 
-# Last but not least. let's check na values
+'''
+STEP 3 - Clean data / Text pre-processing
+'''
+
+# Let's check na values
 print(df.isnull().values.sum())
 
 """
 There are not NA value in this data-set. 
 Based on our data exploration, we knew we can use 'stars' column to classify whether review is Positive or Negative.
 Let's create a new column to store our Class/Label value,
-which depennds on our 'stars' column, if 'stars' is great than 3, Class/Label is 1 - 'Positive'. Otherwise,
+which depends on our 'stars' column, if 'stars' is great than 3, Class/Label is 1 - 'Positive'. Otherwise,
 it is 0 - 'Negative'
 """
 
@@ -310,114 +318,87 @@ else:
     # Import csv
     filter_df = pd.read_csv(review_file_path, encoding='utf-8')
 
-'''
-STEP 3 - Clean data / Text pre-processing
-'''
-
 """
-First of all, let's balance the data
+Next, we would use NLTK method to normalize our corpus.
 """
-balance_review_file_path = get_absolute_path("balance_review.csv")
-
-if not os.path.isfile(balance_review_file_path):
-    # num of Positive record
-    print(filter_df.loc[filter_df["class"] == 1].count())
-
-    # num of Negative record
-    print(filter_df.loc[filter_df["class"] == 0].count())
-
-    # balance the data
-    balance_data_count = 2300
-    n_df = filter_df.loc[filter_df["class"] == 0][:balance_data_count]
-    # number of negative rows
-
-    print("Number of negative should be " + balance_data_count + ". Actual is ", len(n_df.loc[n_df["class"] == 0]))
-
-    print("Number of positive should be 0. Actual is ", len(n_df.loc[n_df["class"] == 1]))
-
-    p_df = filter_df.loc[filter_df["class"] == 1][:balance_data_count]
-    # number of positive rows
-
-    print("Number of positive should be " + balance_data_count + ". Actual is ", len(p_df.loc[p_df["class"] == 1]))
-
-    print("Number of negative should be 0. Actual is ", len(p_df.loc[p_df["class"] == 0]))
-
-    # merge positive and negative together to become a balance data
-    filter_df = n_df.append(p_df)
-
-    filter_df.to_csv(balance_review_file_path, encoding='utf-8', index=False)
-else:
-    # Import csv
-    filter_df = pd.read_csv(balance_review_file_path, encoding='utf-8')
-
-"""
-Secondly, we would use NLTK method to normalize our corpus.
-"""
-
-# Text Normalization - using NLTK
-wpt = nltk.WordPunctTokenizer()
-stop_words = nltk.corpus.stopwords.words('english')
-
-
-def normalize_document(doc):
-    # lower case and remove special characters\whitespaces
-    doc = re.sub(r'[^a-zA-Z0-9\s]', '', doc, re.I)
-    doc = doc.lower()
-    doc = doc.strip()
-    # tokenize document
-    tokens = wpt.tokenize(doc)
-    # filter stopwords out of document
-    filtered_tokens = [token for token in tokens if token not in stop_words]
-    # re-create document from filtered tokens
-    doc = ' '.join(filtered_tokens)
-    return doc
-
-
-normalize_corpus = np.vectorize(normalize_document)
-
-# filter_df["norm_text"] = normalize_corpus(filter_df["text"])
-#
-# # Check the result
-# print(filter_df["norm_text"].describe())
-# print(filter_df.head(1))
-
-"""
-As the result, the norm text is still have some words not fully converted to what we want.
-Such as, 'checked', 'costs', we expected those should stem correctly.
-Next, let's try library Spacy, which provide all lots of helper method for us to normalize our corpus.
-"""
-
-# Text Normalization - using Spacy
-nlp = spacy.load("en_core_web_sm")
-white_list_pos = ["VERB", "PART", "NOUN", "ADJ", "ADV"]
-
-
-def spacy_norm_text(text):
-    # tokenizing
-    doc = nlp(str(text))
-
-    ret_set = set()
-
-    # handle stop words, VERB, PART, ADJ, ADV and NOUN
-    for token in doc:
-        if not token.is_stop and token.text:  # remove stop words & empty string
-            if token.pos_ in white_list_pos:  # if token is in white list, taking lemma_ instead
-                ret_set.add(token.lemma_.lower().strip())
-
-    # handle PROPN
-    for token in doc.ents:
-        ret_set.add(token.text)
-
-    # convert to list
-    unique_list = list(ret_set)
-
-    return " ".join(unique_list)
-
-
 norm_review_file_path = get_absolute_path("norm_review.csv")
 
 if not os.path.isfile(norm_review_file_path):
-    filter_df["norm_text"] = filter_df.apply(lambda row: spacy_norm_text(row["text"]), 1);
+    from string import punctuation
+
+    # Text Normalization - using NLTK
+    wpt = nltk.WordPunctTokenizer()
+    stop_words = nltk.corpus.stopwords.words('english')
+    punctuation_terms = punctuation + '0123456789'
+
+
+    def normalize_document(doc):
+        # lower case and remove special characters\whitespaces
+        doc = re.sub(r'[^a-zA-Z0-9\s]', '', doc, re.I | re.A)
+        doc = doc.lower()
+        doc = doc.strip()
+        # tokenize document
+        tokens = wpt.tokenize(doc)
+
+        filtered_tokens = []
+        for t in tokens:
+            # remove punctuation
+            # filter stopwords out of document
+            if t not in punctuation_terms and t not in stop_words:
+                filtered_tokens.append(t)
+
+        # re-create document from filtered tokens
+        doc = ' '.join(filtered_tokens)
+        return doc
+
+
+    normalize_corpus = np.vectorize(normalize_document)
+
+    filter_df["norm_text_with_order"] = normalize_corpus(filter_df["text"])
+
+    # Check the result
+    print(filter_df["norm_text_with_order"].describe())
+    print(filter_df.head(1))
+
+    """
+    As the result, the norm text is still have some words not fully converted to what we want.
+    Such as, 'checked', 'costs', we expected those should stem correctly.
+    Next, let's try library Spacy, which provide all lots of helper method for us to normalize our corpus.
+    """
+
+    # Text Normalization - using Spacy
+    nlp = spacy.load("en_core_web_sm")
+    white_list_pos = ["VERB", "PART", "NOUN", "ADJ", "ADV"]
+
+
+    def spacy_norm_text(text):
+        # tokenizing
+        doc = nlp(str(text))
+
+        ret_set = set()
+
+        # handle stop words, VERB, PART, ADJ, ADV and NOUN
+        for token in doc:
+            if not token.is_stop and token.text:  # remove stop words & empty string
+                if token.pos_ in white_list_pos:  # if token is in white list, taking lemma_ instead
+                    ret_set.add(token.lemma_.lower().strip())
+
+        # handle PROPN
+        for token in doc.ents:
+            ret_set.add(token.text)
+
+        # convert to list
+        unique_list = list(ret_set)
+
+        return " ".join(unique_list)
+
+
+    filter_df["norm_text"] = filter_df.apply(lambda row: spacy_norm_text(row["text"]), 1)
+
+    # Before save the data to cvs, let's remove the na rows
+    filter_df['norm_text'].dropna(inplace=True)
+    filter_df['norm_text_with_order'].dropna(inplace=True)
+
 
     # Export norm text to file
     filter_df.to_csv(norm_review_file_path, encoding='utf-8', index=False)
@@ -429,25 +410,57 @@ else:
 print(filter_df["norm_text"].describe())
 print(filter_df.head(1))
 
-# Put all word in one set
-words_set = set()
-for doc in filter_df["norm_text"]:
-    for token in doc.split():
-        words_set.add(token)
+'''
+Next, let's balance the data
+'''
+balance_review_file_path = get_absolute_path("balance_review.csv")
+
+if not os.path.isfile(balance_review_file_path):
+    # num of Positive record
+    pos_size = len(filter_df.loc[filter_df["class"] == 1])
+    print(pos_size)
+
+    # num of Negative record
+    neg_size = len(filter_df.loc[filter_df["class"] == 0])
+    print(neg_size)
+
+    # balance the data
+    balance_data_count = 200 #min(pos_size, neg_size)
+    n_df = filter_df.loc[filter_df["class"] == 0][:balance_data_count]
+    # number of negative rows
+
+    print("Number of negative should be " + str(balance_data_count) + ". Actual is ", len(n_df.loc[n_df["class"] == 0]))
+
+    print("Number of positive should be 0. Actual is ", len(n_df.loc[n_df["class"] == 1]))
+
+    p_df = filter_df.loc[filter_df["class"] == 1][:balance_data_count]
+    # number of positive rows
+
+    print("Number of positive should be " + str(balance_data_count) + ". Actual is ", len(p_df.loc[p_df["class"] == 1]))
+
+    print("Number of negative should be 0. Actual is ", len(p_df.loc[p_df["class"] == 0]))
+
+    # merge positive and negative together to become a balance data
+    filter_df = n_df.append(p_df)
+
+    filter_df.to_csv(balance_review_file_path, encoding='utf-8', index=False)
+else:
+    # Import csv
+    filter_df = pd.read_csv(balance_review_file_path, encoding='utf-8')
 
 '''
-STEP 4 - Feature extraction from text
+STEP 4 - Feature engineering
 '''
 
-"""
+'''
 Using TF-IDF to convert text to vector
-"""
+'''
 
 ### TF-IDF
 from sklearn.feature_extraction.text import TfidfVectorizer
 
 vectorizer = TfidfVectorizer(min_df=2)
-tfidf = vectorizer.fit_transform(filter_df["norm_text"].values)
+tfidf = vectorizer.fit_transform(filter_df["norm_text"].values.astype('U'))
 
 # convert to array
 tfidf = tfidf.toarray()
@@ -461,7 +474,7 @@ words = vectorizer.get_feature_names()
 pd.DataFrame(tfidf, columns=words)
 
 '''
-STEP 5 - Build Models
+Build Model
 '''
 
 # Prepare the train and test dataset
@@ -473,7 +486,11 @@ y = filter_df['class'].values  # the labels, or answers, we want to test against
 # split into train and test dataset
 X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.3)
 
-# Logistic regression
+
+'''
+Logistic regression
+'''
+
 from sklearn.linear_model import LogisticRegression
 
 model = LogisticRegression().fit(X_train, y_train)
@@ -487,3 +504,209 @@ y_predict = np.array([int(p[1] > 0.5) for p in predict_ret])
 print(y_predict)
 print(y_test)
 print(np.sum(y_test == y_predict) / len(y_test))
+
+
+'''
+TF-IDF is count-based strategy, which lose additional information such as semantics around the words.
+Next, we would use word embeddings, which can overcome the shortcomings.
+'''
+
+'''
+Using Word2vec
+'''
+
+from gensim.models import word2vec
+
+# tokenize sentences in corpus
+wpt = nltk.WordPunctTokenizer()
+filter_df["norm_text_with_order"].dropna(inplace=True)
+tokenized_corpus = [wpt.tokenize(document) for document in np.array(filter_df["norm_text_with_order"])]
+
+# Set values for various parameters
+feature_size = 100  # Word vector dimensionality
+window_context = 30  # Context window size
+min_word_count = 1  # Minimum word count
+sample = 1e-3  # Downsample setting for frequent words
+
+w2v_model = word2vec.Word2Vec(tokenized_corpus, size=feature_size,
+                              window=window_context, min_count=min_word_count,
+                              sample=sample, iter=50)
+
+# Let's check some most frequency words
+w2v_model.wv.most_similar("great")
+w2v_model.wv.most_similar("hotel")
+# As we seen, most similar word to "hotel" is "venetian", "lobby", "rooms" and so on, which makes sense.
+# And for the most similar word to "great" is "excellent", "awesome", "overservice", "amazing" and so on.
+# This tells us the word2vec model do a very good to transfer word to vector.
+
+# visualize embeddings
+from sklearn.manifold import TSNE
+
+words = w2v_model.wv.index2word
+wvs = w2v_model.wv[words]
+
+tsne = TSNE(n_components=2, random_state=0, n_iter=1000, perplexity=2)
+np.set_printoptions(suppress=True)
+T = tsne.fit_transform(wvs[:1000])
+labels = words
+
+plt.figure(figsize=(12, 6))
+plt.scatter(T[:, 0], T[:, 1], c='orange', edgecolors='r')
+for label, x, y in zip(labels, T[:, 0], T[:, 1]):
+    plt.annotate(label, xy=(x + 1, y + 1), xytext=(0, 0), textcoords='offset points')
+plt.show()
+
+'''
+Build Model - Clustering Word2vec Model
+'''
+
+
+def average_word_vectors(words, model, vocabulary, num_features):
+    feature_vector = np.zeros((num_features,), dtype="float64")
+    nwords = 0.
+
+    for word in words:
+        if word in vocabulary:
+            nwords = nwords + 1.
+            feature_vector = np.add(feature_vector, model[word])
+
+    if nwords:
+        feature_vector = np.divide(feature_vector, nwords)
+
+    return feature_vector
+
+
+def averaged_word_vectorizer(corpus, model, num_features):
+    vocabulary = set(model.wv.index2word)
+    features = [average_word_vectors(tokenized_sentence, model, vocabulary, num_features)
+                for tokenized_sentence in corpus]
+    return np.array(features)
+
+
+# get document level embeddings
+w2v_feature_array = averaged_word_vectorizer(corpus=tokenized_corpus, model=w2v_model,
+                                             num_features=feature_size)
+pd.DataFrame(w2v_feature_array)
+
+from sklearn.cluster import AffinityPropagation
+
+ap = AffinityPropagation()
+ap.fit(w2v_feature_array)
+cluster_labels = ap.labels_
+cluster_labels = pd.DataFrame(cluster_labels, columns=['ClusterLabel'])
+pd.concat([filter_df, cluster_labels], axis=1)
+
+'''
+Using GloVe
+'''
+
+nlp = spacy.load('en_vectors_web_lg')
+total_vectors = len(nlp.vocab.vectors)
+print('Total word vectors:', total_vectors)
+
+unique_words = list(set([word for sublist in [doc.split() for doc in filter_df["norm_text_with_order"]] for word in sublist]))
+
+word_glove_vectors = np.array([nlp(word).vector for word in unique_words])
+w2v_df = pd.DataFrame(word_glove_vectors, index=unique_words)
+
+# visualize embeddings
+from sklearn.manifold import TSNE
+
+tsne = TSNE(n_components=2, random_state=0, n_iter=5000, perplexity=3)
+np.set_printoptions(suppress=True)
+T = tsne.fit_transform(word_glove_vectors[:1000])
+labels = unique_words
+
+plt.figure(figsize=(12, 6))
+plt.scatter(T[:, 0], T[:, 1], c='orange', edgecolors='r')
+for label, x, y in zip(labels, T[:, 0], T[:, 1]):
+    plt.annotate(label, xy=(x+1, y+1), xytext=(0, 0), textcoords='offset points')
+plt.show()
+
+'''
+Build Model - KMeans Clustering
+'''
+
+from sklearn.cluster import KMeans
+
+km = KMeans(n_clusters=3, random_state=0)
+km.fit_transform(word_glove_vectors)
+cluster_labels = km.labels_
+cluster_labels = pd.DataFrame(cluster_labels, columns=['ClusterLabel'])
+glove_df = pd.concat([filter_df, cluster_labels], axis=1)
+
+
+'''
+Using ELMo
+'''
+import tensorflow as tf
+import tensorflow_hub as hub
+import keras
+from sklearn import preprocessing
+
+# Access the elmo model
+url = "https://tfhub.dev/google/elmo/2"
+embed = hub.Module(url)
+
+# Define x and y.
+x = np.array(filter_df["text"])
+y = np.array(filter_df["class"])
+
+# create train and test data
+X_train, X_test, y_train, y_test = train_test_split(x, y, test_size=0.3)
+
+# Import keras layer
+from keras.layers import Input, Lambda, Dense
+from keras.models import Model
+import keras.backend as K
+
+# define a embedding function
+le = preprocessing.LabelEncoder()
+le.fit(y)
+le.classes_
+
+def encode(le, labels):
+    enc = le.transform(labels)
+    return keras.utils.to_categorical(enc)
+
+def decode(le, one_host):
+    dec = np.argmax(one_host, axis=1)
+    return le.inverse_transform(dec)
+
+y_train = encode(le, y_train)
+y_test = encode(le, y_test)
+
+def ELMoEmbedding(x):
+    return embed(tf.squeeze(tf.cast(x,tf.string)), signature="default", as_dict=True)["default"]
+
+
+# Build the model
+input_text = Input(shape=(1,), dtype=tf.string)
+embedding = Lambda(ELMoEmbedding, output_shape=(1024,))(input_text)
+dense= Dense(256, activation='relu')(embedding)
+pred = Dense(2, activation="softmax")(dense)
+model = Model(inputs=[input_text], outputs=pred)
+model.compile(loss='categorical_crossentropy', optimizer="adam", metrics=['accuracy'])
+
+# Train the model
+with tf.Session() as session:
+    K.set_session(session)
+    session.run(tf.global_variables_initializer())
+    session.run(tf.tables_initializer())
+    history = model.fit(X_train, y_train, epochs=1, batch_size=32)
+    model.save_weights("./elmo-model")
+
+# Test the model
+with tf.Session() as session:
+    K.set_session(session)
+    session.run(tf.global_variables_initializer())
+    session.run(tf.tables_initializer())
+    model.load_weights("./elmo-model")
+    predicts = model.predict(X_test, batch_size=32)
+
+
+# Let's take a look the confusion matrix
+from sklearn import metrics
+metrics.confusion_matrix(y_test, predicts)
+
+print(metrics.classification_report(y_test, predicts))
